99f648120d20e17675d81a33279bf6dc202cb990
Aioramu <king-gun@mail.ru>
2023-08-28 22:24:48 +0300

    feat: crud stocks to database

--- stockouter/urls.py
+++ stockouter/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #url(r'counter/$', views.Counter.as_view()),
-    url(r'$', views.FinancialList.as_view()),
-    #url(r'$', views.CatalogRecordsView.as_view()),
-
+    # url(r'counter/$', views.Counter.as_view()),
+    url(r"$", views.FinancialList.as_view()),
+    url(r"stocks", views.StocklList.as_view()),
+    # url(r'$', views.CatalogRecordsView.as_view()),
 ]
